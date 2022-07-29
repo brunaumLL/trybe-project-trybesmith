8c0d35e9891284df052dafbebe6f033feb97fc99
brunaumLL <bruno99louzada@gmail.com>
2022-07-29 17:11:42 -0300

    req 01 feito

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import ProducRouter from './routes/product.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', ProducRouter);
 
 export default app;
 
